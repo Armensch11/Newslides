a3c1b1ed30d17e523e35cb4d10a66ae917fb77bd
Armensch11 <60622581+Armensch11@users.noreply.github.com>
2020-05-19 18:29:55 +0400

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,1 +1,59 @@
+let imageShow = document.getElementsByClassName('nested-image');
+let positionHolder = 0;
 
+let backBtn = document.getElementsByTagName('button')[0];
+let iconBorder = document.getElementsByClassName('icon-image');
+
+backBtn.addEventListener('click', function() {
+	for (image of imageShow) {
+		image.style.zIndex = -1;
+	}
+	for (border of iconBorder) {
+		border.style.border = '1px solid';
+	}
+
+	if (positionHolder === 0) {
+		positionHolder = imageShow.length - positionHolder - 1;
+		imageShow[positionHolder].style.zIndex = 1;
+
+		iconBorder[positionHolder].style.border = '3px solid darkgrey';
+	} else {
+		positionHolder = positionHolder - 1;
+		imageShow[positionHolder].style.zIndex = 1;
+
+		iconBorder[positionHolder].style.border = '3px solid darkgrey';
+	}
+});
+
+let forwardBtn = document.getElementsByTagName('button')[1];
+
+forwardBtn.addEventListener('click', function() {
+	for (image of imageShow) {
+		image.style.zIndex = -1;
+	}
+	for (border of iconBorder) {
+		border.style.border = '1px solid grey';
+	}
+	if (positionHolder === imageShow.length - 1) {
+		positionHolder = 0;
+		imageShow[positionHolder].style.zIndex = 1;
+		iconBorder[positionHolder].style.border = '3px solid yellow';
+	} else {
+		positionHolder = positionHolder + 1;
+		imageShow[positionHolder].style.zIndex = 1;
+		iconBorder[positionHolder].style.border = '3px solid yellow';
+	}
+});
+
+let randomBtn = document.getElementsByTagName('button')[2];
+
+function randomChange() {
+	for (image of imageShow) {
+		image.style.zIndex = -1;
+	}
+	let randomiser = Math.floor(Math.random() * imageShow.length);
+	imageShow[randomiser].style.zIndex = 1;
+}
+randomBtn.addEventListener('click', function() {
+	setInterval((randomChange, 5000));
+});
